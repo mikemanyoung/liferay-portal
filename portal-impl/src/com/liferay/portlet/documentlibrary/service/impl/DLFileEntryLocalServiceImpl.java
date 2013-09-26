c3d5546d96df656ac57870bf2699d68930f7bdde
Everest Liu <everest.liu@liferay.com>
2013-09-25 17:26:29 -0700

    LPS-37352 - Move DL name validation to DLConstants; implement DLConstants name validator to DLStoreImpl; include old chars in 6.1-legacy.props

--- portal-impl/src/com/liferay/portlet/documentlibrary/service/impl/DLFileEntryLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/documentlibrary/service/impl/DLFileEntryLocalServiceImpl.java
@@ -2467,7 +2467,6 @@ public class DLFileEntryLocalServiceImpl
 		}
 
 		validateFileExtension(extension);
-		validateFileName(title);
 
 		DLStoreUtil.validate(title, false);
 
@@ -2503,12 +2502,6 @@ public class DLFileEntryLocalServiceImpl
 			if (extension.length() > maxLength) {
 				throw new FileExtensionException();
 			}
-		}
-	}
-
-	protected void validateFileName(String fileName) throws PortalException {
-		if (!DLFileEntryConstants.isValidName(fileName)) {
-			throw new FileNameException(fileName);
 		}
 	}
 
